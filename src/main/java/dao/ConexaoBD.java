692fb4a36cc06646cf6c5d1b25a0d33cfeca3a1b
rebecagomesferreira <rebecagomes2208@gmail.com>
2025-10-22 19:05:36 -0300

    comentários

--- src/main/java/dao/ConexaoBD.java
+++ src/main/java/dao/ConexaoBD.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 public class ConexaoBD {
     private static final String URL = "jdbc:mysql://localhost:3306/EmpresaDeReciclagem";
     private static final String USER = "root";
-    private static final String PASSWORD = ""; // senha MySQL
+    private static final String PASSWORD = "Beca2285064*"; // senha MySQL
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(URL, USER, PASSWORD); // conexão driver e com o banco
         } catch (SQLException e) {
             System.out.println("Erro ao conectar: " + e.getMessage());
-            return null;
+            return null; // caso não conectar com o banco, retorna uma mensagem de erro e encerra o programa
         }
     }
 }
