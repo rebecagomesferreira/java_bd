67e5ee8d7bf0a90db7e602b576269e82107e841b
Thiago Alves <thiagoalvs10@gmail.com>
2025-10-23 21:24:35 -0300

    Adição de comentários aos Controllers

--- src/main/java/control/FornecedorController.java
+++ src/main/java/control/FornecedorController.java
@@ -4,30 +4,37 @@ import dao.FornecedorDAO;
 import model.Fornecedor;
 import java.util.List;
 
+// controlador para operações de Fornecedor
 public class FornecedorController {
-    private FornecedorDAO dao;
+    private FornecedorDAO dao; // DAO que acessa o banco de dados
 
+    // construtor recebe o DAO que será usado
     public FornecedorController(FornecedorDAO dao) {
         this.dao = dao;
     }
 
+    // cadastrar um fornecedor
     public void cadastrar(Fornecedor f) {
-        dao.inserir(f);
+        dao.inserir(f); // chama DAO para inserir no banco
     }
 
+    // listar todos os fornecedores
     public List<Fornecedor> listar() {
-        return dao.listar();
+        return dao.listar(); // chama DAO para listar do banco
     }
 
+    // buscar fornecedor por ID
     public Fornecedor buscarPorId(int id) {
-        return dao.buscarPorId(id);
+        return dao.buscarPorId(id); // retorna fornecedor ou null
     }
 
+    // atualizar dados de um fornecedor
     public void atualizar(Fornecedor f) {
-        dao.atualizar(f);
+        dao.atualizar(f); // chama DAO para atualizar no banco
     }
 
+    // deletar fornecedor pelo ID
     public void deletar(int id) {
-        dao.deletar(id);
+        dao.deletar(id); // chama DAO para deletar do banco
     }
 }
--- src/main/java/control/MaterialController.java
+++ src/main/java/control/MaterialController.java
@@ -4,30 +4,37 @@ import dao.MaterialDAO;
 import model.Material;
 import java.util.List;
 
+// controlador para operações de Material
 public class MaterialController {
-    private MaterialDAO dao;
+    private MaterialDAO dao; // DAO que acessa o banco de dados
 
+    // construtor recebe o DAO que será usado
     public MaterialController(MaterialDAO dao) {
         this.dao = dao;
     }
 
+    // cadastrar um material
     public void cadastrar(Material m) {
-        dao.inserir(m);
+        dao.inserir(m); // chama DAO para inserir no banco
     }
 
+    // listar todos os materiais
     public List<Material> listar() {
-        return dao.listar();
+        return dao.listar(); // chama DAO para listar do banco
     }
 
+    // buscar material por ID
     public Material buscarPorId(int id) {
-        return dao.buscarPorId(id);
+        return dao.buscarPorId(id); // retorna material ou null
     }
 
+    // atualizar dados de um material
     public void atualizar(Material m) {
-        dao.atualizar(m);
+        dao.atualizar(m); // chama DAO para atualizar no banco
     }
 
+    // deletar material pelo ID
     public void deletar(int id) {
-        dao.deletar(id);
+        dao.deletar(id); // chama DAO para deletar do banco
     }
 }
